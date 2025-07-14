cb5d20f42433693970718d4323a37d107a3c8764
JakkalaRamu2005 <jakkalaramu233@gmail.com>
2025-07-14 10:18:45 +0530

    linked-list

--- c++/JULY/linkedlist-14-7.cpp
+++ c++/JULY/linkedlist-14-7.cpp
@@ -1,29 +1,51 @@
-#include<bits/stdc++.h>
+#include <bits/stdc++.h>
 using namespace std;
 
-class Node{
-    public: 
+class Node {
+public: 
     int data;
     Node* next;
     
-    Node(int val, Node* address){
+    Node(int val, Node* address) {
         data = val;
         next = address;
     }
     
-    Node(int val){
+    Node(int val) {
         data = val;
-        next=nullptr;
+        next = nullptr;
     }
-    Node(){
-        data=0;
-        next=nullptr;
-    }
-}
 
- 
+    Node() {
+        data = 0;
+        next = nullptr;
+    }
+};
+
 int main() {
-  // write your code here...
+    int n;
+    cin >> n;
   
-  return 0;
+    int arr[n];
+  
+    for (int i = 0; i < n; i++) {
+        cin >> arr[i];
+    }
+  
+    Node* head = new Node(arr[0]);
+    Node* curr = head;  
+  
+    for (int i = 1; i < n; i++) {
+        Node* n = new Node(arr[i]);
+        curr->next = n;  
+        curr = n;
+    }
+
+    Node* temp = head;
+    while (temp != nullptr) {
+        cout << temp->data << " ";
+        temp = temp->next;  
+    
+
+    return 0;
 }
