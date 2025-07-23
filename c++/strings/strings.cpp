4b42cd6a44dea34eed70a2e4f0bbe6e7aa6243c3
JakkalaRamu2005 <jakkalaramu233@gmail.com>
2025-07-23 14:15:16 +0530

    added stringfolder

---
+++ c++/strings/strings.cpp
@@ -0,0 +1,17 @@
+#include <bits/stdc++.h>
+using namespace std;
+
+int main(){
+
+    string str = "bitStudents";
+    cout << str.length() << endl;
+
+    str.push_back('!');
+    str.pop_back();
+    cout << str << endl;
+
+    cout << str.find('u') << endl; // returns the first occurrence of the char or word
+
+    
+
+}
