c6dc3fecb92bbdcd2134c34f61ce912fbeb7d335
JakkalaRamu2005 <jakkalaramu233@gmail.com>
2025-08-08 23:22:36 +0530

    reason

---
+++ c++/recursions/generalizedfibonocci.cpp
@@ -0,0 +1,24 @@
+#include <bits/stdc++.h>
+using namespace std;
+int  fibonocci(int i){
+    if()
+}
+vector<int> getGeneralizedFibonocci(int k, int n, vector<int>&arr){
+    for(int i=k; i<=n; i++){
+        for(int j=0; j<k; j++){
+
+        }
+
+    }
+
+}
+// don't look at the solution and try to comeup with your own bootforce appraoch and solve the problem that you wanted to solve
+// I request you to not go for the quick fixes, understand what's happening why it's happening
+// core foundamentals is what gonna make you can expert. understanding why and how things work is very important.
+
+int main(){
+    int k=3;
+    int n=6;
+    vector<int> arr={0,1,2};
+    getGeneralizedFibonocci(k,n,arr);
+}
