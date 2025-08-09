374998a25c155fec88a3d3a1645ed759fd828ced
JakkalaRamu2005 <jakkalaramu233@gmail.com>
2025-08-09 22:24:33 +0530

    tried

--- c++/recursions/generalizedfibonocci.cpp
+++ c++/recursions/generalizedfibonocci.cpp
@@ -1,24 +1,27 @@
 #include <bits/stdc++.h>
 using namespace std;
-int  fibonocci(int i){
-    if()
-}
-vector<int> getGeneralizedFibonocci(int k, int n, vector<int>&arr){
-    for(int i=k; i<=n; i++){
-        for(int j=0; j<k; j++){
 
+vector<int> generateGeneralizedFibonacci(int k, vector<int>& start, int n) {
+        // Code Here
+        
+        
+        // if(k==n){
+        //     return;
+        // }
+        for(int i=0; i<k; i++){
+            int num=0;
+            for(int j=0; j<i+k; j++){
+                int num+=start[i];
+            }
+            cout << num;
         }
-
+        // generateGeneralizedFibonacci(k+1,vector<int>& start,n);
+         
     }
-
-}
-// don't look at the solution and try to comeup with your own bootforce appraoch and solve the problem that you wanted to solve
-// I request you to not go for the quick fixes, understand what's happening why it's happening
-// core foundamentals is what gonna make you can expert. understanding why and how things work is very important.
 
 int main(){
     int k=3;
     int n=6;
     vector<int> arr={0,1,2};
-    getGeneralizedFibonocci(k,n,arr);
+    generateGeneralizedFibonacci(k,arr,n);
 }
