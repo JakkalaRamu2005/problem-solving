9b49d82810b80dfd9e4403eb3b89d9fa1f6f127a
JakkalaRamu2005 <jakkalaramu233@gmail.com>
2025-10-15 08:16:42 +0530

    valid anagram

--- c++/strings/praticequestion.cpp
+++ c++/strings/praticequestion.cpp
@@ -1,6 +1,7 @@
 #include <bits/stdc++.h>
 using namespace std;
 
+
 int main(){
 
     // below is the brute force approach to solve this problem
@@ -29,9 +30,24 @@ int main(){
 int arr[26]={0};
 // int size = sizeof(arr)/sizeof(arr[0]);
 // cout << size << endl;
-
+// we are initilizing arr to 0
 for(int i=0; i<s1.size(); i++){
+    arr[s1[i]-'a']+=1;
     
+}
+for(int j=0; j<s2.size(); j++){
+    arr[s2[j]-'a']-=1;
+}
+bool isAnagram=true;
+for (int j:arr){
+    if(j!=0){
+        cout << "Not a valid anagram" << endl;
+        isAnagram=false;
+        break;
+    }
+}
+if(isAnagram){
+    cout << "valid Anagram" << endl;
 }
 
     return 0;
