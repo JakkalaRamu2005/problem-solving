3d6df50f666bd4896d6cf3549711d77bfcec8900
JakkalaRamu2005 <jakkalaramu233@gmail.com>
2025-10-23 06:51:26 +0530

    22-10

---
+++ daily-coding/october-2025/twosum.cpp
@@ -0,0 +1,30 @@
+#include <bits/stdc++.h>
+using namespace std;
+
+int main(){
+
+    ic:
+    vector<int> twoSum(vector<int>& nums, int target) {
+        int n = nums.size();
+        vector<int> index;
+
+        for (int i = 0; i < n; i++) {
+            for (int j = i + 1; j < n; j++) {
+                if (nums[i] + nums[j] == target) {
+                    index.push_back(i);
+                    index.push_back(j);
+                    return index;
+                }
+            }
+        }
+
+        // if no pair found
+        return {};
+    }
+
+
+
+
+
+    return 0;
+}
