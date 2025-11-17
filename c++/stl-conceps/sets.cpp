b00f8a563ea7cf1f33ad53be37cf434666498aff
JakkalaRamu2005 <jakkalaramu233@gmail.com>
2025-11-17 19:50:31 +0530

    added stlfolder

---
+++ c++/stl-conceps/sets.cpp
@@ -0,0 +1,45 @@
+#include <bits/stdc++.h>
+using namespace std;
+
+int main() {
+	// your code goes here
+	
+
+
+set<int> s = {5,10,15,20,25,30};
+// s.insert(35);
+// s.insert(10);
+// s.insert(22);
+
+//insert
+//earase 
+//clear
+//empty
+//swap 
+auto it1 = s.find(20);
+auto it2 = s.find(35);
+
+// s.erase(it1, it2);
+	
+	
+// for(auto x: s) cout << x << " ";
+	
+	
+	
+// for(auto it = s.begin(); it!=s.end(); it++){
+    
+//     cout << *it << " ";
+    
+    
+// }
+
+// s.clear(); clear all the elements in the set
+
+
+if(s.empty()){
+    cout << "Set is empty";
+}
+
+
+
+}
