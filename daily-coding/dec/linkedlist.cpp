62810da7271817b310d5c9e15b27c82cec81f1f0
JAKKALA RAMU <jakkalaramu233@gmail.com>
2025-12-21 22:04:10 +0530

    Create linkedlist.cpp

---
+++ daily-coding/dec/linkedlist.cpp
@@ -0,0 +1,35 @@
+#include <bits/stdc++.h>
+using namespace std;
+
+class Node{
+    public:
+        int data;
+        Node* next;
+        
+        Node(int value, Node* address){
+            data = value;
+            next = address;
+        }
+        
+        Node(int value){
+            data = value;
+            next = nullptr;
+        }
+        
+        Node(){
+            data=0;
+            next = nullptr;
+        }
+};
+
+
+
+int main() {
+	// your code goes here
+
+
+
+
+
+
+}
