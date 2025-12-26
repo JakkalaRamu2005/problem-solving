df7a67f5581ef37c032ac227f5794da3ffe5ecc7
JAKKALA RAMU <jakkalaramu233@gmail.com>
2025-12-27 00:06:14 +0530

    Update linkedlist.cpp

--- daily-coding/dec/linkedlist.cpp
+++ daily-coding/dec/linkedlist.cpp
@@ -15,21 +15,41 @@ class Node{
             data = value;
             next = nullptr;
         }
-        
         Node(){
-            data=0;
-            next = nullptr;
+            data =0;
+            next=nullptr;
         }
 };
 
 
 
+Node* arrtoLL(vector<int> arr, int n){
+    if(n==0) return 0;
+    
+    Node* head = new Node(arr[0]);
+    Node* cur = head;
+    for(int i=1; i<n; i++){
+        cur->next = new Node(arr[i]);
+        cur= cur->next;
+    }
+    return head;
+}
+
 int main() {
 	// your code goes here
+	vector<int> arr = {1,2,3,4,5};
+	int n = arr.size();
+	
+	Node* head = arrtoLL(arr, n);
+	
+    Node* temp = head;
+    while(temp!=nullptr){
+        cout << temp->data << " ";
+        
+        temp = temp->next;
+    }
+	
 
-
-
-
-
+	
 
 }
