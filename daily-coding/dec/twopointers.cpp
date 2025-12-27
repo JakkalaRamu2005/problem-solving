af060cda672dcb6ae4c79f00b9fc1488ef7b366f
JAKKALA RAMU <jakkalaramu233@gmail.com>
2025-12-28 00:04:17 +0530

    Add two pointers implementation in twopointers.cpp
    Implement two pointers technique to find minimum length subarray.

---
+++ daily-coding/dec/twopointers.cpp
@@ -0,0 +1,21 @@
+#include <bits/stdc++.h>
+using namespace std;
+
+int main() {
+	// your code goes here
+	
+	int start=0;
+	int sum=0;
+	int min_Len = INT_MAX;
+	
+	for(int i=0; i<arr.size(); i++){
+	    sum+=arr[i];
+	    
+	    while(sum>=target){
+	        minLen = min(minLen, end-start+1);
+	        
+	    }
+	    
+	}
+
+}
