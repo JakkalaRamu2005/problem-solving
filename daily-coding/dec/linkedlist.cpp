934691b585a68d31e1ffb654b314a552c34f5de2
JAKKALA RAMU <jakkalaramu233@gmail.com>
2026-01-04 23:59:45 +0530

    Refactor linked list and add utility functions
    Refactored linked list implementation, added utility functions.

--- daily-coding/dec/linkedlist.cpp
+++ daily-coding/dec/linkedlist.cpp
@@ -1,66 +1,8 @@
-#include <bits/stdc++.h>
+#include<bits/stdc++.h>
 using namespace std;
 
 class Node{
-    public:
-        int data;
-        Node* next;
-        
-        Node(int value, Node* address){
-            data = value;
-            next = address;
-        }
-        
-        Node(int value){
-            data = value;
-            next = nullptr;
-        }
-        Node(){
-            data =0;
-            next=nullptr;
-        }
-};
-
-
-
-Node* arrtoLL(vector<int> arr, int n){
-    if(n==0) return 0;
-    
-    Node* head = new Node(arr[0]);
-    Node* cur = head;
-    for(int i=1; i<n; i++){
-        cur->next = new Node(arr[i]);
-        cur= cur->next;
-    }
-    return head;
-}
-
-int main() {
-	// your code goes here
-	vector<int> arr = {1,2,3,4,5};
-	int n = arr.size();
-	
-	Node* head = arrtoLL(arr, n);
-	
-    Node* temp = head;
-    while(temp!=nullptr){
-        cout << temp->data << " ";
-        
-        temp = temp->next;
-    }
-	
-
-	
-
-}
-
-
-#include <bits/stdc++.h>
-using namespace std;
-    
-class Node{
-    public:
-    int data;
+    public: int data;
     Node* next;
     
     Node(int value, Node* address){
@@ -75,58 +17,50 @@ class Node{
         data = 0;
         next = nullptr;
     }
-};  
-
-
-Node* arrtoLL(int arr[], int n){
-    
-    Node* head = new Node(arr[0]);
-    Node* cur = head;
-    for(int i=1; i<n; i++){
-       Node* tem = new Node(arr[i]);
-       cur->next = tem;
-        cur = cur->next;
+};
+void printLL(Node* head){
+    Node* temp = head;
+    while(temp!=nullptr){
+        cout << temp->data << " ";
+        temp = temp->next;
+        // temp->next = temp;
     }
-    
+}
+bool searchinLL(Node* head, int value){
+    Node* temp = head;
+    while(temp!=nullptr){
+        if(temp->data==value){
+            return true;
+        }
+        temp = temp->next;
+    }
+    return false;
+}
+int countLL(Node* head){
+    Node* temp = head;
+    int count=0;
+    while(temp!=nullptr){
+        temp = temp->next;
+        count++;
+    }
+    return count;
+}
+Node* arrtoLL(int arr[], int n){
+    Node* head = new Node(arr[0]);
+    Node* temp = head;
+    for(int i=1; i<n; i++){
+        temp->next = new Node(arr[i]);
+        temp = temp->next;
+    }
     return head;
-    
-    
 }
-
 int main() {
-	// your code goes here
-	
-	// input a->5
-	// 20->30->40->50->null 
-	
-	//-5->30->40->50->null 
-	
-    // inserting a new at the head 
-  
-    
-
-int arr[]={1,3,4,5};
-int n = sizeof(arr)/sizeof(arr[0]);
-
-// cout << n<< endl;
-
-
-
-// cout << head->data << endl;
-
-Node* head = arrtoLL(arr,n);
-
-cout << head->data << endl;
-Node* temp = head;
-
-while(temp!=nullptr){
-    cout << temp->data <<" ";
-    temp=temp->next;
+  // write your code here...
+  int arr[]={1,2,3,4};
+  int n = sizeof(arr)/sizeof(arr[0]);
+  Node* head = arrtoLL(arr, n);
+//   printLL(head);
+//   cout << countLL(head);
+// cout << searchinLL(head,2);
+  return 0;
 }
-return 0;
-
-
-    
-
-}
-
