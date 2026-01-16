89a3948baab93d254e0b9b44535c87b26ee4b0ae
JAKKALA RAMU <jakkalaramu233@gmail.com>
2026-01-16 19:23:35 +0530

    Update findunion.cpp

--- daily-coding/jan/findunion.cpp
+++ daily-coding/jan/findunion.cpp
@@ -16,3 +16,36 @@ for(auto key: count){
 return result;
     }
 };
+
+
+#include<bits/stdc++.h>
+using namespace std;
+ 
+ 
+ vector<int> union_arrays(vector<int> a, vector<int> b){
+     unordered_set<int> result;
+     vector<int> uni;
+     for(auto i: a){
+         result.insert(i);
+     }
+     for(auto j: b){
+         result.insert(j);
+     }
+     for(auto el: result){
+         uni.push_back(el);
+     }
+     return uni;
+ }
+
+int main() {
+  // write your code here...
+//   [1, 2, 3, 2, 1], b[] = [3, 2, 2, 3, 3, 2]
+    vector<int> a = {1, 2, 3, 2, 1};
+    vector<int> b = {3,2,2,3,3,2};
+    
+    vector<int> ans = union_arrays(a,b);
+    for(auto x: ans){
+        cout << x << " ";
+    }
+  return 0;
+}
