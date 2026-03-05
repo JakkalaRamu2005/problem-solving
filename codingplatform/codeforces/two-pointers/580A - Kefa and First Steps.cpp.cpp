6568c19cd7e828e4d0b314f5b077b987b29d0835
JAKKALA RAMU <jakkalaramu233@gmail.com>
2026-03-05 12:19:46 +0530

    Update 580A - Kefa and First Steps.cpp.cpp

--- codingplatform/codeforces/two-pointers/580A - Kefa and First Steps.cpp.cpp
+++ codingplatform/codeforces/two-pointers/580A - Kefa and First Steps.cpp.cpp
@@ -1,6 +1,63 @@
 #include <bits/stdc++.h>
 using namespace std;
 
+int bruteForce(vector<int>& arr) {
+    int n = arr.size();
+    int ans = 1;
+
+    for(int i = 0; i < n; i++) {
+        int len = 1;
+
+        for(int j = i + 1; j < n; j++) {
+            if(arr[j] >= arr[j - 1]) {
+                len++;
+                ans = max(ans, len);
+            } else {
+                break;
+            }
+        }
+    }
+
+    return ans;
+}
+
+int optimal(vector<int>& arr) {
+    int longest = 1;
+    int current = 1;
+
+    for(int i = 1; i < arr.size(); i++) {
+
+        if(arr[i] >= arr[i-1]) {
+            current++;
+        } else {
+            current = 1;
+        }
+
+        longest = max(longest, current);
+    }
+
+    return longest;
+}
+
+int main() {
+
+    int n;
+    cin >> n;
+
+    vector<int> arr(n);
+
+    for(int i = 0; i < n; i++) {
+        cin >> arr[i];
+    }
+
+    cout << optimal(arr);
+
+}
+
+
+#include <bits/stdc++.h>
+using namespace std;
+
 int main() {
 	// your code goes here
 	int n; cin>>n; 
