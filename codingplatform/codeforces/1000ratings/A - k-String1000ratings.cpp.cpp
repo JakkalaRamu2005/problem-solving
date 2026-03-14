3534759d0dc0e4cc187705a2b33f36fe481c128c
JAKKALA RAMU <jakkalaramu233@gmail.com>
2026-03-14 12:31:10 +0530

    Update A - k-String1000ratings.cpp.cpp

--- codingplatform/codeforces/1000ratings/A - k-String1000ratings.cpp.cpp
+++ codingplatform/codeforces/1000ratings/A - k-String1000ratings.cpp.cpp
@@ -46,3 +46,53 @@ int main() {
 	return 0;
 
 }
+
+
+/*optimized solution using the hashing  */
+#include <bits/stdc++.h>
+using namespace std;
+
+int main() {
+	// your code goes here
+	
+string st="abcdefghijklmnopqrstuvwxyz";
+int k; cin>>k;
+string s; cin>>s;
+
+vector<int> count(26);
+
+for(char ch: s){
+    // cout << ch-'a' << endl;
+    
+    count[ch-'a']++;
+    
+}
+
+string ans="";
+bool flag = true;
+
+for(int i=0; i<26; i++){
+    
+    if(count[i]%k!=0){
+        flag = false;
+        cout << -1;
+        break;
+    }else{
+        int rep = count[i]/k;
+        for(int j=0; j<rep; j++){
+            ans+=st[i];
+        }
+    }
+    
+}
+
+if(flag){
+    for(int i=0; i<k; i++){
+        cout << ans;
+    }
+}
+
+
+
+return 0;
+}
